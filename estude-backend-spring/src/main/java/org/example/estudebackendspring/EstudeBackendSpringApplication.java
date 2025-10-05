4c3e03144c267a211fb3a9a3e523255b4d05ddeb
DinhNguyenChung <dinhvanchung24042003@gmail.com>
2025-10-05 16:32:36 +0700

    fix dot env

--- estude-backend-spring/src/main/java/org/example/estudebackendspring/EstudeBackendSpringApplication.java
+++ estude-backend-spring/src/main/java/org/example/estudebackendspring/EstudeBackendSpringApplication.java
@@ -8,6 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstudeBackendSpringApplication {
 
 	public static void main(String[] args) {
+		// Load .env từ root project (nơi có build.gradle)
+		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv
+				.configure()
+				.directory("./")          // root project
+				.ignoreIfMissing()
+				.load();
+
+		// Ghi biến vào System để Spring @Value đọc được
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue())
+		);
+
+		// Chạy Spring Boot
+
 		SpringApplication.run(EstudeBackendSpringApplication.class, args);
 	}
 
